eb29adde1a46ed65583e14d61c7adc479081a97c
victormuchunu-cyber <victor.muchunu@gmail.com>
2026-04-10 13:31:02 +0300

    updated code

---
+++ app/src/main/java/com/example/sokogarden/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.sokogarden
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+//        Find buttons by use of their ids.
+        val signupButton= findViewById<Button>(R.id.signupBtn)
+        val SigninButton = findViewById<Button>(R.id.signinBtn)
+
+//        Create the intent to the two activities.
+        signupButton.setOnClickListener {
+            val intent = Intent(applicationContext, Signup::class.java)
+            startActivity(intent)
+        }
+
+        //        =====================================
+        SigninButton.setOnClickListener {
+            val intent = Intent(applicationContext, Signin::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
